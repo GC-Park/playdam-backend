ee9762f8b10061c48d4b9ba92f9e102238bf1d0f
GC-Park <kuyt3122@naver.com>
2024-04-14 15:36:12 +0900

    feat: add typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
 import { AppController } from './app.controller';
 import { BoardsModule } from './boards/boards.module';
+import { typeORMConfig } from './boards/configs/typeorm.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MoviesModule, BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), MoviesModule, BoardsModule],
   controllers: [AppController],
   providers: [],
 })
