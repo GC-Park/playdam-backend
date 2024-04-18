7f6e4535566e56f9e11d7579856c662275ec498f
GC-Park <kuyt3122@naver.com>
2024-04-18 15:09:15 +0900

    feat: create auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { BoardsModule } from './boards/boards.module';
 import { typeORMConfig } from './configs/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), MoviesModule, BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), MoviesModule, BoardsModule, AuthModule],
   controllers: [AppController],
   providers: [],
 })
