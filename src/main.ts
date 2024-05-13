d987b8798eab05dcee4e80a55250f1ca311a8496
GC-Park <kuyt3122@naver.com>
2024-05-13 17:43:03 +0900

    refactor: use config in port number

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+  const serverConfig = config.get('server');
+  const port = serverConfig.port;
 
   app.useGlobalPipes(
     new ValidationPipe({
